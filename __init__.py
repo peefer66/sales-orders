56b517e0f83f2cdcedcca084008f09f6f98ec861
peefer66 <peefer66@hotmail.com>
2017-01-24 15:57:32 +0000

    added .gitignore

--- __init__.py
+++ __init__.py
@@ -1,4 +1,18 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate
+from flask.ext.markdown import Markdown
+
+app = Flask(__name__)
+
+# dtatbase
+app.config.from_object('settings')
+db = SQLAlchemy(app)
+
+# Migrate
+migrate = Migrate(app,db)
+
+# Markdown
+markdown = Markdown(app)
 
 
-app = Flask("__name__")
